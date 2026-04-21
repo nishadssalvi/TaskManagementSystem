f264d45fc9281ead3401f96e89636558c39596bb
Nishad Salvi <nishadssalvi@gmail.com>
2026-04-21 15:51:01 +0530

    Add files via upload

---
+++ src/main/java/com/taskManagement/ManagementSystem/dto/TaskDTO.java
@@ -0,0 +1,17 @@
+package com.taskManagement.ManagementSystem.dto;
+
+public class TaskDTO {
+    private String title;
+    private String description;
+    private boolean completed;
+
+    // Getters & Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public boolean isCompleted() { return completed; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
+}
